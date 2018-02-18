ed01142336fc59dce7ebe23bb2c6371d48ac96cb
Kosate Limpongsa <kosatelim@gmail.com>
2018-02-18 18:32:02 +0700

    🐛 Fix main.js file

--- backend/main.js
+++ backend/main.js
@@ -1,9 +1,10 @@
-const express = require('express')
-const app = express()
-
+const express = require('express');
 const mongoose = require('mongoose');
+
+const app = express();
+
 mongoose.connect('mongodb://db/battleship');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
